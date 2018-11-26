5dd0f7b812529ff683a6a1d7f527896c946927c9
yuanjihuang <yh3059@columbia.edu>
2018-11-25 21:26:23 -0500

    third commit

--- main.py
+++ main.py
@@ -9,25 +9,39 @@ from guizero import App,Text,PushButton
 import Adafruit_ADS1x15
 import Adafruit_ADXL345
 
-def reader(i):
-    global stop
+
+class Sensor:
+    def __init__(self):
+        self.adc = Adafruit_ADS1x15.ADS1115(address = 0x48)
+        self.acel = Adafruit_ADXL345.ADXL345(address = 0x53)   
+    def read(self):
+        muscle = self.adc.read_adc(0, gain=1)
+        x,y,z = self.acel.read()
+        return (muscle,x,y,z)
+    
+    
+def repeater():
     if stop == 1:
-        data.value = i
-        data.after(500,reader,args = [i+1])
+        muscle,x,y,z = sensor.read()
+        data.value = ('muscle={0},x={1},y={2},z={3}'.format(muscle,x,y,z))
+        data.after(500,repeater)
     else:
         data.value = 'success!'
+        
 def calibrate():
     global stop
     stop = 1
-    data.after(500,reader,args = [1])
+    data.after(500,repeater)
+    
 def train():
     pass
 def stop_func():
     global stop
     stop = 0
+    
 if __name__ == "__main__":
-    adc = Adafruit_ADS1x15.ADS1115(address = 0x48)
-    acel = Adafruit_ADXL345.ADXL345(address = 0x53)
+    sensor = Sensor()
+    muscle,x,y,z = sensor.read()
     app = App(title="AI Trainer")
     welcome_message = Text(app, text="Please choose mode")
     calibrate = PushButton(app, command=calibrate, text="Calibrate")
@@ -36,14 +50,5 @@ if __name__ == "__main__":
     stop = 0
     data = Text(app, text="")
     app.display()
-    '''while True:
-        
-        values = adc.read_adc(0, gain=1)
-        x,y,z = acel.read()
-        #gyro = adafruit_l3gd20.L3GD20_I2C(i2c)
-        print(values)
-        print('x={0},y={1},z={2}'.format(x,y,z))
-        #print(gyro.gyro)
-        # Pause for half a second.
-        time.sleep(0.3)'''
+    
 
