46e62ccb243a18b270a1bf64e0041edbf02df29f
yuanji huang <huangyuanji12@yeah.net>
2018-11-26 16:02:01 -0500

    graph

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ Created on Sun Nov 25 17:11:09 2018
 
 @author: yuanjihuang
 """
+import time
 from guizero import App,Text,PushButton,Picture
 import Adafruit_ADS1x15
 import Adafruit_ADXL345
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 class Sensor:
-    def __init__(self,max_len):
+    def __init__(self,max_len = 20):
         self.adc = Adafruit_ADS1x15.ADS1115(address = 0x48)
         self.acel = Adafruit_ADXL345.ADXL345(address = 0x53)   
         self.cur_time = 0
@@ -36,28 +37,47 @@ class Sensor:
         x,y,z = self.acel.read()
         self.acc.append((x,y,z))
         #check length
+        #print(self.muscle,self.acc)
         self.check_len()
+        return (self.muscle, self.acc)
     def create_image(self):
-        self.read()
         plt.figure()
         plt.plot(self.muscle)
         plt.savefig('tem.png')
         plt.close()
+        #print('image created!')
+        #print(self.muscle)
         return 'tem.png'
     
 def repeater(sensor):
     if stop == 1:
+        #print('stop is 1')
+        sensor.read()
         fig_name = sensor.create_image()
-        picture.image = fig_name
-        picture.after(500,repeater,args=[sensor])
+        picture.value = fig_name
+        picture.after(200,repeater,args=[sensor])
+        #print('next round')
     else:
         print('end')
+
+def data_repeater(sensor):
+    global stop
+    if stop == 1:
+        #print('stop is 1')
+        muscle,acc = sensor.read()
+        data.value = str(muscle)
+        data.after(1000,data_repeater,args=[sensor])
+        #print('next round')
+    else:
+        print('data_end')
+        
         
 def calibrate():
     global stop
-    sensor = Sensor()
+    sensor = Sensor(100)
     stop = 1
-    picture.after(500,repeater,args=[sensor])
+    #data.after(500,data_repeater,args=[sensor])
+    picture.after(50,repeater,args=[sensor])
     
 def train():
     pass
@@ -66,14 +86,14 @@ def stop_func():
     stop = 0
     
 if __name__ == "__main__":
-    plt.ioff()
     app = App(title="AI Trainer")
     welcome_message = Text(app, text="Please choose mode")
     calibrate = PushButton(app, command=calibrate, text="Calibrate")
     train = PushButton(app, command=train, text="Train")
     stop_btn = PushButton(app, command=stop_func, text="stop")
     stop = 0
-    picture = Picture(app, image="")
+    data = Text(app, text = "123")
+    picture = Picture(app, image="white.png",width=300,height=250)
     app.display()
     
 
